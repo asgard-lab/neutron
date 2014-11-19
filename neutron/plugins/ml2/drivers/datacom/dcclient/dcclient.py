7704bd56ce48a38167fe79233c5d3a94472f440a
zemeio <tonhoanhon@gmail.com>
2014-11-19 19:51:49 +0100

    Add per switch config file support.
    Changed update and create network to iterate on each switch for the changes.
    Now the XML information is saved on the switch dictionary.

--- neutron/plugins/ml2/drivers/datacom/dcclient/dcclient.py
+++ neutron/plugins/ml2/drivers/datacom/dcclient/dcclient.py
@@ -16,8 +16,6 @@ class Manager:
     def __init__(self):
         self.switches_dic = {}
 
-        self.xml = ManagedXml()
-
     def setup(self):
         # Setup specific configurations
         oslo_parser = cfg.MultiConfigParser()
@@ -49,17 +47,21 @@ class Manager:
                                     str(switch),
                                     str(sw_dic['dm_method']))
 
+            sw_dic['xml'] = ManagedXml()
             self.switches_dic[switch] = sw_dic
 
     def _update(self):
         for switch in self.switches_dic:
-            self.switches_dic[switch]['rpc'].send_xml(self.xml.tostring())
+            xml = self.switches_dic[switch]['xml']
+            self.switches_dic[switch]['rpc'].send_xml(xml.tostring())
 
     def create_network(self, vlan, name=''):
         """ Creates a new network on the switch, if it does not exist already.
         """
         try:
-            self.xml.addVlan(vlan, name=name)
+            for switch in self.switches_dic:
+                xml = self.switches_dic[switch]['xml']
+                xml.addVlan(vlan, name=name)
             self._update()
         except:
             LOG.info("Trying to create already existing network %d:", vlan)
